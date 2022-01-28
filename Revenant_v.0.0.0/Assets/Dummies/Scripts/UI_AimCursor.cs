e643fca3cbb5b160059982ddaf7adbf66ac4f26d
HJ <meiroo2@daum.net>
2022-01-29 03:07:22 +0900

    에임&총알 임시 추가

--- Revenant_v.0.0.0/Assets/Dummies/Scripts/UI_AimCursor.cs
+++ Revenant_v.0.0.0/Assets/Dummies/Scripts/UI_AimCursor.cs
@@ -7,6 +7,9 @@ public class UI_AimCursor : MonoBehaviour
     // Member Variables
     private RectTransform m_rect;
 
+    public GameObject m_bullet;
+    public GameObject m_gun;
+
     // Constructors
     private void Awake()
     {
@@ -16,5 +19,11 @@ public class UI_AimCursor : MonoBehaviour
     private void Update()
     {
         m_rect.position = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(m_bullet);
+            bullet.transform.SetPositionAndRotation(m_gun.transform.position, m_gun.transform.rotation);
+        }
     }
 }
