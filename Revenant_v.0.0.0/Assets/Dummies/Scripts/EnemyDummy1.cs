3e8235afedd23c1da64704872e8266a4e1fb6ac7
HJ <meiroo2@daum.net>
2022-01-29 03:22:11 +0900

    New Merge Update

---
+++ Revenant_v.0.0.0/Assets/Dummies/Scripts/EnemyDummy1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyDummy1 : EnemyDummy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = 3;
+        //Debug.Log(3);
+        MoveToX(h);
+        ChangeColor(Color.cyan);
+    }
+
+    private void Awake()
+    {
+        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Debug.Log(getYPos());
+        StartCoroutine(MoveXTo(3));
+
+        // y축 값이 -10보다 작으면 리스폰
+        respawn();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveYInput();
+    }
+
+    //IEnumerator MoveXTo(int n)
+    //{
+    //    yield return new WaitForSeconds(0.1f);
+    //    transform.position = new Vector2(n, transform.position.y);
+    //}
+}
