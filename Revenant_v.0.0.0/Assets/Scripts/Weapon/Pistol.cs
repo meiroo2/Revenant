022d178891a465a710402cb7fcc77c37302c0901
HJ <meiroo2@daum.net>
2022-02-23 00:39:29 +0900

    플레이어 총기 교체 기능 구현 완료
    + 총염 이펙트 위치만 잡음

---
+++ Revenant_v.0.0.0/Assets/Scripts/Weapon/Pistol.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : BASEWEAPON
+{
+    // Visible Member Variables
+
+
+    // Member Variables
+
+
+    // Constructors
+    private void Awake()
+    {
+
+    }
+    private void Start()
+    {
+
+    }
+    /*
+    <커스텀 초기화 함수가 필요할 경우>
+    public void Init()
+    {
+
+    }
+    */
+
+    // Updates
+    private void Update()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+
+    }
+
+    // Physics
+
+
+    // Functions
+    public override void Fire()
+    {
+        if (m_PlayerGun.m_canShot)
+        {
+            GameObject InstancedBullet = Instantiate(m_BulletPrefab);
+            Player_Bullet InstancedBullet_Script = InstancedBullet.GetComponent<Player_Bullet>();
+
+            if (m_Player.m_isRightHeaded)
+                InstancedBullet_Script.InitBullet(m_BulletSpeed, m_BulletDamage);
+            else
+                InstancedBullet_Script.InitBullet(-m_BulletSpeed, m_BulletDamage);
+
+            InstancedBullet.transform.SetPositionAndRotation(m_Player_Arm.position, m_Player_Arm.rotation);
+            InstancedBullet_Script.m_aimedObjId = m_aimCursor.AimedObjid;
+        }
+    }
+
+    // 기타 분류하고 싶은 것이 있을 경우
+}
