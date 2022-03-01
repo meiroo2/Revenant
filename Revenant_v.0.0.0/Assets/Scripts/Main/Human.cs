3065571ca9f006ea91f2c7aec737184743ffce0f
KMJ512 <minji010402@gmail.com>
2022-03-02 06:09:35 +0900

    적 시각 감지, 사격 완료

--- Revenant_v.0.0.0/Assets/Scripts/Main/Human.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Main/Human.cs
@@ -14,7 +14,7 @@ public class Human : ObjectDefine
 {
     // Member Variables
     [field: SerializeField] public humanState m_curHumanState { get; private set; } = humanState.Live;
-    [field: SerializeField] public float m_Hp { get; private set; } = 10;
+    [field: SerializeField] public float m_Hp { get; protected set; } = 10;
     [field: SerializeField] public float m_Speed { get; private set; } = 1f;
     [field: SerializeField] public float m_stunTime { get; private set; } = 0f;
     [field: SerializeField] public bool m_hasStun { get; private set; } = false;
