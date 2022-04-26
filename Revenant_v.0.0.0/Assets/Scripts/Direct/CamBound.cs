96a2564a59481927a26da09e356a1517d549d41a
meiroo2 <meiroo2@daum.net>
2022-04-26 15:38:11 +0900

    카메라 바운더리 설정

---
+++ Revenant_v.0.0.0/Assets/Scripts/Direct/CamBound.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBound : MonoBehaviour
+{
+    // 0, 1, 2, 3 -> ╩С го аб ©Л
+    public int m_BanDirection = 0;
+}
