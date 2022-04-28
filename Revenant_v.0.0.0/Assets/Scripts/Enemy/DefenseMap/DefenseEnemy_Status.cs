76a9516a60f13a8ad52caa979472319ec77f900c
KMJ512 <minji010402@gmail.com>
2022-04-28 23:02:22 +0900

    인스펙터 표시 완료

--- Revenant_v.0.0.0/Assets/Scripts/Enemy/DefenseMap/DefenseEnemy_Status.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Enemy/DefenseMap/DefenseEnemy_Status.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 public class DefenseEnemy_Status : Enemy
 {
     // 추격 선딜
-    [field: SerializeField]
+    //[field: SerializeField]
     public float m_preChaseTime { get; set; } = 1.0f;
 
     // 시야 (추격)
-    [field: SerializeField]
+    //[field: SerializeField]
     public float m_guardSightDistance { get; set; } = 2.0f;    // 추격 거리
     // 사정 거리 (전투)
+    [field: Header("사정 거리")]
     [field: SerializeField]
     public float m_fightSightDistance { get; set; } = 1.5f;   // 공격 거리
 
     // 경직정도
-    [field: SerializeField]
+    //[field: SerializeField]
     public float m_stunStack { get; set; } = 0;
 
     // 사격 선딜
+    [field: Header("사격 딜레이")]
     [field: SerializeField]
     public float m_preFightTime { get; set; } = 1.0f;
     
