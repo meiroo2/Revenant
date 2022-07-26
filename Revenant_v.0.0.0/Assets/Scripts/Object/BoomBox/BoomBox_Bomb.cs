5e8b70da41cb3859f7f5f7d4e59fe469bdbfee77
meiroo2 <meiroo2@daum.net>
2022-07-26 15:54:40 +0900

    폭발 드럼통 완성
    (리소스 수정 필요)

---
+++ Revenant_v.0.0.0/Assets/Scripts/Object/BoomBox/BoomBox_Bomb.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+public class BoomBox_Bomb : MonoBehaviour
+{
+    // Member Variables
+    private Dictionary<Collider2D, IHotBox> m_Dic = new Dictionary<Collider2D, IHotBox>();
+    
+    
+    // Functions
+    public void Explode(int _damage, int _stunValue)
+    {
+        foreach (var hotBox in m_Dic.Values)
+        {
+            hotBox.HitHotBox(new IHotBoxParam(_damage, _stunValue,
+                transform.position, WeaponType.BULLET));
+        }
+    }
+    
+    
+    // Physics
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        IHotBox colHotbox = col.GetComponent<IHotBox>();
+
+        if (colHotbox.m_HitBoxInfo == HitBoxPoint.BODY)
+            m_Dic.Add(col, colHotbox);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        m_Dic.Remove(other);
+    }
+}
