7e6209ccb13847d519e95b74235d5cd1db4faf2c
RememberTAE <2vlgofk@gmail.com>
2022-09-19 16:08:43 +0900

    적 비활성화 이후 체크포인트 Enemy List 실시간 반영

--- Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint_Col.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint_Col.cs
@@ -20,19 +20,25 @@ public class CheckPoint_Col : MonoBehaviour, IUseableObj
 
     public void ActivateOutline(bool _isOn)
     {
+        if (_checkPoint.EnemyListToActivate.Count == 0)
+        {
+            return;
+        }
+        else
+        {
+            for (int i = 0; i < _checkPoint.EnemyListToActivate.Count; i++)
+            {
+                if (_checkPoint.EnemyListToActivate[i].activeSelf == false)
+                {
+                    _checkPoint.EnemyListToActivate.RemoveAt(i);
+                }
+            }
+        }
         _checkPoint.ActivateBothOutline(_isOn);
     }
     
     public int useObj(IUseableObjParam _param)
     {
-        for (int i = 0; i < _checkPoint.EnemyListToActivate.Count; i++)
-        {
-            if (_checkPoint.EnemyListToActivate[i].activeSelf == false)
-            {
-                _checkPoint.EnemyListToActivate.RemoveAt(i);
-            }
-        }
-        
         // 성공하면 1, 실패하면 0을 반환
         // If player interacted Checkpoint, activate it
         if (_checkPoint.bCanInteract == true && _checkPoint.EnemyListToActivate.Count == 0)
