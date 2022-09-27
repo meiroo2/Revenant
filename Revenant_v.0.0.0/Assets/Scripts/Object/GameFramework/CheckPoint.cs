344fe16b666e39527e4294cd5b17421f95257435
RememberTAE <2vlgofk@gmail.com>
2022-09-27 13:52:31 +0900

    체크포인트 최적화, 자잘한 버그 수정

--- Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Schema;
 using Sirenix.OdinInspector;
+using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -20,8 +21,11 @@ public class CheckPoint : MonoBehaviour
     // private Animator thisAnimator;
 
     /** List with all checkpoints objects in the scene */
-    public static List<GameObject> CheckPointsList;
+    public static List<CheckPoint> CheckPointsList;
 
+    private GameObject test;
+    private CheckPoint Test_2;
+    
     [Header("체크포인트 활성화 조건 버튼 - 맵 배치 적")] public bool bEnemyListToActivate;
     public List<BasicEnemy> EnemyListToActivate;
 
@@ -46,7 +50,7 @@ public class CheckPoint : MonoBehaviour
         //thisAnimator = GetComponent<Animator>();
 
         // 현재 씬에 있는 모든 체크포인트를 찾기 (이름 순으로 정렬)
-        CheckPointsList = GameObject.FindGameObjectsWithTag("CheckPoint").OrderBy(CP => CP.name).ToList();
+        CheckPointsList = FindObjectsOfType<CheckPoint>().OrderBy(TCP => TCP.name).ToList();
 
         SetUpSectionNumber();
         SetUpIsActivated();
@@ -62,18 +66,17 @@ public class CheckPoint : MonoBehaviour
             EnemyListFromSpawner = new List<GameObject>(EnemyListToActivateFromSpawner[i].p_WillSpawnEnemys);
         }
     }
-
+    
     /** 섹션 번호 초기화 함수 */
     void SetUpSectionNumber()
     {
         // 각 체크포인트에 섹션 번호 부여
         for (int i = 0; i < CheckPointsList.Count; i++)
         {
-            CheckPointsList[i].GetComponent<CheckPoint>().SectionNumber = i + 1;
+            CheckPointsList[i].SectionNumber = i + 1;
         }
     }
-
-    /** 씬 로드 시 체크포인트 활성화 여부 판별 함수 */
+    
     void SetUpIsActivated()
     {
         // 저장된 데이터 중 체크포인트가 활성화 되어있다면 체크포인트의 섹션을 판별
@@ -81,8 +84,7 @@ public class CheckPoint : MonoBehaviour
             DataHandleManager.Instance.CheckPointSectionNumber == SectionNumber)
         {
             // 해당 체크포인트를 활성화
-            CheckPointsList[SectionNumber - 1].GetComponent<CheckPoint>().bActivated =
-                DataHandleManager.Instance.IsCheckPointActivated;
+            CheckPointsList[SectionNumber - 1].bActivated = DataHandleManager.Instance.IsCheckPointActivated;
         }
     }
 
@@ -90,17 +92,14 @@ public class CheckPoint : MonoBehaviour
     void DestroyAssignedObjects()
     {
         // 체크포인트가 활성화 되어있고 섹션 번호가 해당되는 체크포인트와 같거나 작은 체크포인트들을 확인 후
-        if (bActivated && SectionNumber <= CheckPointsList[SectionNumber - 1].GetComponent<CheckPoint>().SectionNumber)
+        if (bActivated && SectionNumber <= CheckPointsList[SectionNumber - 1].SectionNumber)
         {
             for (int i = 0; i < SectionNumber; i++)
             {
-                // 스포너가 있는 체크포인트 일 때 스포너 내부 적 제거
-                if (bEnemyListToActivateFromSpawner)
-                {
-                    DestroyAssignedObjectsList(CheckPointsList[i].GetComponent<CheckPoint>().EnemyListFromSpawner);
-                }
+                // 스포너 내부 적 제거
+                DestroyAssignedObjectsList(CheckPointsList[i].EnemyListFromSpawner);
                 // 등록된 오브젝트 제거 (스포너 등록 가능)
-                DestroyAssignedObjectsList(CheckPointsList[i].GetComponent<CheckPoint>().AssignedObjectsList);
+                DestroyAssignedObjectsList(CheckPointsList[i].AssignedObjectsList);
             }
         }
     }
@@ -130,10 +129,10 @@ public class CheckPoint : MonoBehaviour
         if (CheckPointsList != null)
         {
             // 모든 체크포인트 찾은 뒤
-            foreach (GameObject CheckPointGameObject in CheckPointsList)
+            foreach (CheckPoint CheckPointGameObject in CheckPointsList)
             {
                 // 활성화된 체크포인트를 찾아서
-                if (CheckPointGameObject.GetComponent<CheckPoint>().bActivated)
+                if (CheckPointGameObject.bActivated)
                 {
                     // 체크포인트 위치를 결과값에 반환
                     result = CheckPointGameObject.transform.position;
@@ -149,12 +148,11 @@ public class CheckPoint : MonoBehaviour
     public void ActivateCheckPoint()
     {
         // 씬에 있는 모든 체크포인트를 비활성화
-        foreach (GameObject CheckPointGameObject in CheckPointsList)
+        foreach (CheckPoint CheckPointGameObject in CheckPointsList)
         {
-            CheckPointGameObject.GetComponent<CheckPoint>().bActivated = false;
+            CheckPointGameObject.bActivated = false;
             //cp.GetComponent<Animator>().SetBool("Active", false);
         }
-
         // 현재 체크포인트 활성화
         bActivated = true;
         // 체크포인트 활성화 여부 저장
