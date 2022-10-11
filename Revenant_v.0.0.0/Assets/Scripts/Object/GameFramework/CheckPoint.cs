151253f5a1859077e9cba517f507ff7326bf429f
chldhkswn3 <chldhkswn6@naver.com>
2022-10-11 10:33:20 +0900

    10_11_CWJ
    테스트 레벨 제작 및 수치조정

--- Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Object/GameFramework/CheckPoint.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Schema;
 using Sirenix.OdinInspector;
-using Unity.PlasticSCM.Editor.WebApi;
 using Unity.VisualScripting;
 using UnityEngine;
 
