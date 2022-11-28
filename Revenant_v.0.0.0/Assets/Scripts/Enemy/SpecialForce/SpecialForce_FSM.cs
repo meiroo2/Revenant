8e3a4b37d9d8794609e7df54a64be7895f0502c2
RememberTAE <2vlgofk@gmail.com>
2022-11-28 15:52:14 +0900

    Revert "스페셜포스 문 사용 버그 고침"
    This reverts commit 1e9e0eafd43f12428715fc4f250cd07a13fd5c27.

--- Revenant_v.0.0.0/Assets/Scripts/Enemy/SpecialForce/SpecialForce_FSM.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Enemy/SpecialForce/SpecialForce_FSM.cs
@@ -312,11 +312,6 @@ public class SpecialForce_FOLLOW : SpecialForce_FSM
         if(!m_Enemy.IsFacePlayer())
             m_Enemy.setisRightHeaded(!m_Enemy.m_IsRightHeaded);
 
-        if (m_Enemy.IsSameFloorWithPlayer(m_Enemy.bMoveToUsedDoor, m_Enemy.bIsOnStair, m_Enemy.m_Player.bIsOnStair))
-        {
-            m_Enemy.MoveToPlayer();
-        }
-        
         switch (m_Phase)
         {
             case 0:
@@ -359,6 +354,11 @@ public class SpecialForce_FOLLOW : SpecialForce_FSM
                     else
                     {
                         m_Enemy.SetRigidByDirection(!(m_Enemy.transform.position.x > m_Enemy.m_Player.transform.position.x));
+                    }
+
+                    if (m_Enemy.IsSameFloorWithPlayer(m_Enemy.bMoveToUsedDoor, m_Enemy.bIsOnStair, m_Enemy.m_Player.bIsOnStair))
+                    {
+                        m_Enemy.MoveToPlayer();
                     }
                 }
                 break;
@@ -639,10 +639,11 @@ public class SpecialForce_ATTACK : SpecialForce_FSM
 
     public override void UpdateState()
     {
-        if (m_Enemy.IsSameFloorWithPlayer(m_Enemy.bMoveToUsedDoor, m_Enemy.bIsOnStair, m_Enemy.m_Player.bIsOnStair))
-        {
-            m_Enemy.MoveToPlayer();
-        }
+        // if (m_Enemy.IsSameFloorWithPlayer(m_Enemy.bMoveToUsedDoor, m_Enemy.bIsOnStair, m_Enemy.m_Player.bIsOnStair))
+        // {
+        //     //m_Enemy.ChangeEnemyFSM(EnemyStateName.FOLLOW);
+        //     m_Enemy.MoveToPlayer();
+        // }
         
         m_Distance = m_Enemy.GetDistanceBetPlayer();
 
