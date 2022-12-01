59494b8bb84804dc726cd956823987e73e172e78
RememberTAE <2vlgofk@gmail.com>
2022-12-01 22:20:58 +0900

    부숴지는 박스들 리소스 적용 및 물리 처리

--- Revenant_v.0.0.0/Assets/Scripts/Object/Breakable/Breakable_Hotbox.cs
+++ Revenant_v.0.0.0/Assets/Scripts/Object/Breakable/Breakable_Hotbox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,18 +12,27 @@ public class Breakable_Hotbox : MonoBehaviour, IHotBox
     public int m_hotBoxType { get; set; } = 0;
     public bool m_isEnemys { get; set; } = false;
     public HitBoxPoint m_HitBoxInfo { get; set; } = HitBoxPoint.OBJECT;
- 
+    
+    private Player_UI PlayerUI;
+
     
     // Constructors
     private void Awake()
     {
         m_ParentObj = this.gameObject;
     }
-    
+
+    private void Start()
+    {
+        var Instance = InstanceMgr.GetInstance();
+        PlayerUI = Instance.m_Player_UI;
+    }
+
     // Functions
     public int HitHotBox(IHotBoxParam _param)
     {
         Breakable.GetHit(_param.m_Damage);
+        PlayerUI.ActiveHitmark(1);
         return 1;
     }
 }
